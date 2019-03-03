4a5322072f9832d64d58afbf536b26f222f98f8d
nhatchung14 <48084423+nhatchung14@users.noreply.github.com>
2019-03-04 00:18:11 +0700

    Add files via upload

---
+++ src/Menu.java
@@ -0,0 +1,35 @@
+
+import org.lwjgl.input.Mouse;
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
+
+public class Menu extends BasicGameState{
+	public Menu(int state) {
+		
+	}
+	
+	public void init(GameContainer gc, StateBasedGame game) throws SlickException {
+		
+	}
+	
+	public void render(GameContainer gc, StateBasedGame game, Graphics g) throws SlickException {
+		g.fillOval(75, 100, 100, 100);
+		g.drawString("Play!", 105, 75);
+	}
+	
+	public void update(GameContainer gc, StateBasedGame game, int delta) throws SlickException {
+		Input input = gc.getInput();
+		
+		int xPos = Mouse.getX();
+		int yPos = Mouse.getY();
+		if ((xPos >= 75 && xPos <= 175) && (yPos >= 520 && yPos <= 620)) {
+			if (input.isMouseButtonDown(0)) {
+				game.enterState(1);
+			}
+		}
+	}
+	
+	public int getID() {
+		return 0;
+	}
+}
