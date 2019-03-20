3b1eab2a3bb7607229e9700f8e841d5601773730
minhluanlqd <39330110+minhluanlqd@users.noreply.github.com>
2019-03-20 14:39:13 +0700

    Add files via upload

---
+++ src/characters/BlueGhost.java
@@ -0,0 +1,34 @@
+package characters;
+
+import org.newdawn.slick.Animation;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
+public class BlueGhost extends Ghost {
+
+
+    public BlueGhost(String imageDir, String color, String extension) throws SlickException {
+        super(imageDir,color, extension);
+
+//        Image[] ghostWalkRight = new Image[2];
+//        Image [] ghostWalkDown = new Image[2];
+//        Image [] ghostWalkLeft = new Image[2];
+//        Image [] ghostWalkUp = new Image[2];
+//
+//        for (int img = 0; img < 2; img++)	{
+//            ghostWalkRight[img] = new Image(imageDir + "blue-ghost/lookright" + extension);
+//            ghostWalkDown[img]  = new Image(imageDir + "blue-ghost/lookdown" + extension);
+//            ghostWalkLeft[img]  = new Image(imageDir + "blue-ghost/lookleft" + extension);
+//            ghostWalkUp[img]    = new Image(imageDir + "blue-ghost/lookup" + extension);
+//        }
+//
+//
+//        ghostUp = new Animation(ghostWalkRight, ghostDuration, true);
+//        ghostDown = new Animation(ghostWalkDown, ghostDuration, true);
+//        ghostLeft = new Animation(ghostWalkLeft, ghostDuration, true);
+//        ghostRight = new Animation(ghostWalkUp, ghostDuration, true);
+//
+//        ghost = ghostDown;
+
+    }
+}
