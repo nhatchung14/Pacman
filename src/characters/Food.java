a8d7478aedbf49a3c14f3fe6ad5a6a04cef37fc0
minhluanlqd <39330110+minhluanlqd@users.noreply.github.com>
2019-04-25 16:10:16 +0700

    Add files via upload

--- src/characters/Food.java
+++ src/characters/Food.java
@@ -1,22 +1,44 @@
 package characters;
 
+import board.Size;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
 public class Food {
-    private int col;
-    private int row;
-    private Image food;
+    private float foodPosX;
+    private float foodPosY;
 
-
-    public Food(int col, int row){
-        this.col = col;
-        this.row = row;
+    public float getFoodPosX() {
+        return foodPosX;
     }
 
-    public void init() throws SlickException {
-        food = new Image("image/food/food.png");
-
+    public void setFoodPosX(float foodPosX) {
+        this.foodPosX = foodPosX;
     }
+
+    public float getFoodPosY() {
+        return foodPosY;
+    }
+
+    public void setFoodPosY(float foodPosY) {
+        this.foodPosY = foodPosY;
+    }
+
+    // Draw food func
+    public void drawFood()throws SlickException{
+        Image food = new Image("image/food/food.png");
+        //food.draw(50, 50);
+        //food.draw(50, 150);
+        for (int i = 0; i < 18;i++){
+            for (int j = 0; j < 30;j++){
+                if (Size.getInstance().board_matrix[i][j] != -1){
+                    food.draw(10 + j*50,10 + i*50); // j is x axis, i is y axis
+                    setFoodPosX(10 + j*50);
+                    setFoodPosY(10 + i*50);
+                }
+            }
+        }
+    }
+
 
 }
